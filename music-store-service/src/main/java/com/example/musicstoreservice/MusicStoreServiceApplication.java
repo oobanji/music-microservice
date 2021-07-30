5079fd39bffa0b664bddf9d20d9db88ea8751681
s8baolor <s9baolor@stud.uni-saarland.de>
2021-07-31 00:05:13 +0200

    new music store microservice project

---
+++ music-store-service/src/main/java/com/example/musicstoreservice/MusicStoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicstoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicStoreServiceApplication.class, args);
+    }
+
+}
